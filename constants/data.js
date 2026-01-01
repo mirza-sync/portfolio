16958a6f3cd89405e5de8089491fdcdd0c84ff0e
Mirza <mirzasyahmi123@gmail.com>
2026-01-01 11:32:12 +0800

    Update resume URL

--- constants/data.js
+++ constants/data.js
@@ -156,7 +156,7 @@ const userData = {
     },
   ],
   resumeUrl:
-    "https://drive.google.com/file/d/1XsBU6Er2vrJl8F-y7SuaobESQpghv2XJ/view?usp=sharing",
+    "https://drive.google.com/file/d/1tBEjSd2pTvyoE2fs-uYoPVf6OTqnHtZf/view?usp=sharing",
   socialLinks: {
     linkedin: "https://www.linkedin.com/in/mirza-syahmi/",
     github: "https://github.com/mirza-sync",
